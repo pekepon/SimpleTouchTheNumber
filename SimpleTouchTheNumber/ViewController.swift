73a5e3230ba0c8c7d82a220f9cf52e2ad92e753f
井上圭一 <inoue@inter-reuse.co.jp>
2016-02-06 16:13:44 +0900

    Initial Commit

---
+++ SimpleTouchTheNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTouchTheNumber
+//
+//  Created by 井上圭一 on 2016/02/06.
+//  Copyright © 2016年 keiichi, inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
